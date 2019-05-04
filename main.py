bd4537e402b41284861992cd0d219b3b07ddb1fa
lockness-Ko <videolk9373@gmail.com>
2019-05-04 12:37:14 +1000

    writing the pixel values to an output file (test) next commit will be for json

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ img = Image.open('D:/Science Mentors 2019/ImageProcesser/pointillist.bmp', 'r')
 #Getting the pixel values
 pixelVals = list(img.getdata())
 
-#Writing it to the console
-print(pixelVals)
+#Writing new file
+out = open("out.txt",'x')
+out.write(pixelVals)
